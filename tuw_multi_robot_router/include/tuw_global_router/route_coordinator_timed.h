cecc7c9041678a6939fd0ccc0ed3bf1722d8795e
Kara Q <quast@gestalt-robotics.com>
2024-04-13 17:07:09 +0100

    Debug global object inclusion 4

--- tuw_multi_robot_router/include/tuw_global_router/route_coordinator_timed.h
+++ tuw_multi_robot_router/include/tuw_global_router/route_coordinator_timed.h
@@ -31,7 +31,7 @@
 
 #include <tuw_global_router/route_coordinator.h>
 #include <tuw_global_router/srr_utils.h>
-#include <tuw_global_router/router_node.h>
+//#include <tuw_global_router/router_node.h>
 
 namespace multi_robot_router
 {
