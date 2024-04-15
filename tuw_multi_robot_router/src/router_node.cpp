f028b9f6fab2afef78cc53f88eb2cb7daa124cd9
Kara Q <quast@gestalt-robotics.com>
2024-04-15 09:04:39 +0100

    Debug coordinator 12

--- tuw_multi_robot_router/src/router_node.cpp
+++ tuw_multi_robot_router/src/router_node.cpp
@@ -152,10 +152,10 @@ void Router_Node::existingPathsCallback ( const nav_msgs::Path &msg ) {
     //existingPathSegVectors.clear(); cant do this cuz itll then only have the last path it receives in it
    //populate vector with vectors containing 2d vector values and starting positions of steps in the path
     std::vector<std::vector<float>> temp_vect;
-    ROS_INFO('length of msg: ' + std::to_string(msg.poses.size()));
+    std::cout << "length of msg: " << std::to_string(msg.poses.size());
     if (msg.poses.size() != 0){
         for(int i = 1; i < msg.poses.size(); i++) {
-            std::cout << 'IN FIRST LOOP';
+            std::cout << "IN FIRST LOOP";
             std::vector<float> step;
             float x_end_pos = msg.poses[i].pose.position.x;
             float y_end_pos = msg.poses[i].pose.position.y;
@@ -182,18 +182,18 @@ void Router_Node::existingPathsCallback ( const nav_msgs::Path &msg ) {
             //std::cout << "THE CONVERTED PATH  FROM DB" << str;
         }
     }
-    std::cout << 'MADE IT OUT OF FIRST LOOP';
+    std::cout << "MADE IT OUT OF FIRST LOOP";
     //ROS_INFO(temp_vect);
 
     if (temp_vect.size() != 0){
         for (int i = 0; i < temp_vect.size(); i++){
-            std::cout << 'IN SECOND LOOP';
+            std::cout << "IN SECOND LOOP";
             //std::cout << "inserting. map size: " << std::to_string(existingPathSegVectors.size());
             existingPathSegVectors.insert({{i-3, i-3}, temp_vect[i]});
         }
     }
 
-    std::cout << 'MADE IT PAST SECOND LOOP';
+    std::cout << "MADE IT PAST SECOND LOOP";
     float segTime = 3.5;
     //bool allPathSegsFound = false;
 
@@ -235,9 +235,9 @@ void Router_Node::existingPathsCallback ( const nav_msgs::Path &msg ) {
         }
     }
 
-    std::cout << 'GOT HERE';
-    std::cout << 'pathCount in routernode: ' << std::to_string(pathCount);
-    std::cout << 'number of existing paths: ' << std::stoi(msg.header.frame_id);
+    std::cout << "GOT HERE";
+    std::cout << "pathCount in routernode: " << std::to_string(pathCount);
+    std::cout << "number of existing paths: " << std::stoi(msg.header.frame_id);
     pathCount = pathCount + 1;
     if (pathCount == std::stoi(msg.header.frame_id)){
         allPathSegsFound = true;
